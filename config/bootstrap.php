292d8404aff533a93686421353ffb141286ae3f9
benny <bmclyes1995@gmail.com>
2022-10-11 17:17:18 +0200

    frame

---
+++ config/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+use Dotenv\Dotenv;
+
+
+    // Chargement de l'autoloader de composer
+    require_once __DIR__ . "/../vendor/autoload.php";
+
+
+    // Chargement des variables d'environnement
+    $dotenv = Dotenv::createImmutable(dirname(__DIR__));
+    $dotenv->load();
+
+
+    // Chargement du conteneur de dépendances
+    $container = require_once __DIR__ . "/dependenciesInjection/container.php";
---
+++ public/index.php
@@ -0,0 +1,35 @@
+<?php
+
+
+    /**
+     * ----------------------------------------------------
+     * Bienvenue dans notre framework fait maison
+     * 
+     * L'index.php représente le "FrontController"
+     * Ses rôles : 
+     *              - Charger le fichier de configuration
+     *              - Créer une nouvelle instance du noyau
+     *              - Récupérer la réponse qui sera ensuite 
+     *                envoyée au navigateur
+     * ----------------------------------------------------
+    */
+
+    
+    // Chargement du fichier de configuration
+    require_once dirname(__DIR__) . "/config/bootstrap.php";
+    
+
+    // Création d'une nouvelle instance du noyau de l'application
+    $kernel = new App\Kernel($container);
+
+
+    /**
+     * Le frontController demande au noyau de soummetrre la requête
+     * et de récupérer la réponse correspondante
+     */
+    $response = $kernel->handleRequest();
+
+
+
+    // Envoi de la réponse au client
+        $response->send();
